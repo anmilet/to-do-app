8248755bc02f8c0518e6cb9e8009f1f9571bc683
Miletza <anmilet@gmail.com>
2023-11-24 20:14:04 -0500

    To Do App Initial

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+import { LabsComponent } from './pages/labs/labs.component';
+
+
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent
+    },
+    {
+        path:'labs',
+        component: LabsComponent
+    }
+];
---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  welcome='Hola a Todos'
+  tasks=[
+    'Instalar el Angular CLI',
+    'Crear Proyecto',
+    'Crear Componentes'
+  ];
+  name='Miletza';
+  age= '40';
+  private id='1451537636'; /* esta es una variable privada y no se puede acceder desde el html, solo de este componente */
+  disabled=true;
+  imag='https://picsum.photos/200';
+
+  person = {
+    name:'Luis',
+    photo:'https://w3schools.com/howto/img_avatar.png',
+    age:20
+  }
+  
+}
