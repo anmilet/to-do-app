b40b235c6b1ab008e6eaba620e82a811bfc25fc0
Miletza <anmilet@gmail.com>
2023-11-30 18:30:40 -0500

    ngFor with task of type object

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Task } from './../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -9,20 +10,39 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks=signal([
-    'Instalar el Angular CLI',
-    'Crear Proyecto',
-    'Crear Componente',
-    'Crear Servicio'
+  tasks=signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Crear Proyecto',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear Componente',
+      completed: false
+    }
   ]);
   
   changeHandler(event: Event){
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
+    this.addTask(newTask);
+  }
+
+  addTask(title: string)
+  {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false
+    };
     this.tasks.update((tasks)=> [...tasks, newTask]);
   }
 
   deleteTask(index: number){
-    this.tasks.update((tasks) => tasks.filter((task, position) => position!== index));
+    this.tasks.update(
+      (tasks) => tasks.filter(
+        (task, position) => position !== index
+        ));
   }
 }
