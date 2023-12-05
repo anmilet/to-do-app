9cf6c4d8b4d82e0b098d17388d5e68777b71ec18
Miletza <anmilet@gmail.com>
2023-12-04 20:00:37 -0500

    Controlling an input

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule} from '@angular/forms'
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
@@ -16,6 +17,18 @@ export class LabsComponent {
     'Crear Componente',
     'Crear Servicio'
   ]);
+
+  colorCtrl = new FormControl;
+
+  constructor()
+  {
+    this.colorCtrl.valueChanges.subscribe(value =>
+      {
+        console.log(value);
+      }
+    )
+  }
+
   name=signal('Miletza');
   age= '40';
   private id='1451537636'; /* esta es una variable privada y no se puede acceder desde el html, solo de este componente */
